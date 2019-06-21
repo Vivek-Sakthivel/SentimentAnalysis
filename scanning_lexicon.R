9b53e5de8a1b185907886cd03fbcf774d8baf6b2
Vivek-Sakthivel <44678922+Vivek-Sakthivel@users.noreply.github.com>
2019-06-21 01:10:34 +0100

    To scan and create lexicons library

---
+++ scanning_lexicon.R
@@ -0,0 +1,5 @@
+#After downloading the hui.lui.pos and hui.lui.neg, mention below the location of the file
+#Make sure you edit the location
+
+positive.words = scan('B:/Sentiment_Analysis/positive-lexicons.txt', what='character', comment.char=';') 
+negative.words = scan('B:/Sentiment_Analysis/negative-lexicons.txt', what='character', comment.char=';')
